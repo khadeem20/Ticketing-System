a482f3e9f9ffd4271a608513954032fd1c66baa4
Khadeem Lattibeaudiere <khadeemlatti20@gmail.com>
2025-02-20 16:22:32 -0500

    Login functionality completed and sign up page created

--- ticketsystem/tickets/urls.py
+++ ticketsystem/tickets/urls.py
@@ -20,8 +20,11 @@ from tickets import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', views.home, name="home"),
+    path('', views.home, name="home"),
+    path("ticket/login", views.login_page, name="login-page"),
+     path("ticket/signup", views.signup_page, name="signup-page"),
     #Ticket Management
     path('ticket/create-ticket', views.create_ticket, name="create_ticket"),
+
 
 ]
--- ticketsystem/tickets/views.py
+++ ticketsystem/tickets/views.py
@@ -1,9 +1,28 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def home(request):
     return render(request,"home.html")
+
+def login_page(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        password= request.POST['password']
+        user= authenticate(request, username=username, passwword=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+
+    return render(request, "login.html")
+def signup_page(request):
+    return render(request, "signup.html")
+
+
 
 #Ticket Management Functions
 def create_ticket(request):  # Create new support tickets
